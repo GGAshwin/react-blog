66a4b1dbfeaa11ca2b56cb170da109f5b17fc021
playmaker159 <ashwinprabhu2001@gmail.com>
2022-10-31 13:21:11 +0530

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const Post = require('./database/models/Post');
 const path = require("path");
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -82,7 +82,7 @@ app.get('/api/test', (req, res) => {
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
     app.get("*", function (_, res) {
         res.sendFile(
-            path.join(__dirname, "/frontend/build/index.html"),
+            path.join(__dirname, "/client/build/index.html"),
             function (err) {
                 if (err) {
                     res.status(500).send(err);
